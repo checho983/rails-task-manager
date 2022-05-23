3c369fc6bf12f3ceb67bc870413571dbf2afff6f
sergiososa <smatias.sosa@gmail.com>
2022-05-22 22:04:38 -0300

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'tasks/index', to: 'tasks#index'
- # get 'tasks/new', to: 'tasks#new'
- # get 'tasks/create', to: 'tasks#create'
-  get ‘tasks/:id’, to: ‘tasks#show’, as: :show
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
